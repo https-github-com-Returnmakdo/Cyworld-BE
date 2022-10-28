22adc09206e91ef771384c1cfe96a519dedb827c
JangKroed <didlsdydgh@gmail.com>
2022-10-28 23:57:43 +0900

    일촌평 작성자 데이터 email -> userId로 변경

--- services/ilchonpyungs.services.js
+++ services/ilchonpyungs.services.js
@@ -6,12 +6,15 @@ class IlchonpyungsService {
     const { ilchonpyung } = req.body;
     const { userId } = req.params;
     // const { user } = res.locals;
-    // if (userId === user.userId)
-    //   throw new Error('내 미니홈피에는 일촌평 작성이 불가합니다.');
+
     const user = {
       name: '홍길동',
-      email: 'test1234@gmail.com',
+      userId: 1,
     };
+
+    // if (userId === user.userId)
+    //   throw new Error('내 미니홈피에는 일촌평 작성이 불가합니다.');
+
     if (!ilchonpyung) throw new Error('일촌평을 작성해주세요.');
     if (ilchonpyung.length < 3)
       throw new Error('일촌평을 3자이상 입력해주세요.');
@@ -19,7 +22,7 @@ class IlchonpyungsService {
     return await this.ilchonpyungsRepository.createBest({
       userId,
       name: user.name,
-      writerEmail: user.email,
+      writerId: user.userId,
       ilchonpyung,
     });
   };
@@ -34,17 +37,17 @@ class IlchonpyungsService {
     const { userId, ilchonId } = req.params;
     // const { user } = res.locals;
     const user = {
-      email: 'test1234@gmail.com',
+      userId: 1,
     };
 
     const best = await this.ilchonpyungsRepository.findByBest(ilchonId);
 
     if (!best) throw new Error('존재하지 않는 일촌평입니다.');
 
-    if (best.writerEmail !== user.email)
+    if (best.writerId !== user.userId)
       throw new Error('본인이 작성한 일촌평이 아닙니다.');
 
-    await this.ilchonpyungsRepository.deleteBest(userId, ilchonId);
+    await this.ilchonpyungsRepository.deleteBest( userId, ilchonId );
   };
 }
 module.exports = IlchonpyungsService;
