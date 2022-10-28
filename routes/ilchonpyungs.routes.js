3ae0507b506f9675826bc32791050cfb91fc2f63
JangKroed <didlsdydgh@gmail.com>
2022-10-28 23:57:10 +0900

    일촌평 기능 구현에따라 라우터 개방

--- routes/ilchonpyungs.routes.js
+++ routes/ilchonpyungs.routes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const IlchonpyungsController = require('../controllers/ilchonpyungs.controllers');
 const ilchonpyungsController = new IlchonpyungsController();
 
-router.route('/:userId').post(ilchonpyungsController.createBest);
-//   .get(ilchonpyungsController.getBests)
-//   .delete(ilchonpyungsController.deleteBest);
+router.route('/:userId/:ilchonId').delete(ilchonpyungsController.deleteBest);
+
+router
+  .route('/:userId')
+  .post(ilchonpyungsController.createBest)
+  .get(ilchonpyungsController.getBests);
 
 module.exports = router;
