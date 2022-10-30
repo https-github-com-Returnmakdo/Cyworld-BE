e2c613fd37fb42800d966ece4c70beeb39bb8f1a
hyunjin9603 <hyunjin9603@gmail.com>
2022-10-30 15:14:53 +0900

    intro까지

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authlogin = require('../middlewares/authLoginUserMiddleware');
+const auth = require('../middlewares/authMiddlewares');
 const UsersController = require('../controllers/users.controllers');
 const usersController = new UsersController();
 
@@ -9,5 +10,5 @@ router.post('/login',authlogin,usersController.login)
 router.get('/emailcheck',usersController.emailCheck)
 router.get('/surfing', usersController.surfing);
 router.get('/myhome/:userId', usersController.myhome);
-
+router.put('/myhome/:userId',auth,usersController.intro);
 module.exports = router;
