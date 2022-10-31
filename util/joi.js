e84a7ea3633ff8296232f81669de52788bcf511c
JangKroed <didlsdydgh@gmail.com>
2022-10-31 10:25:27 +0900

    상세 수정

--- util/joi.js
+++ util/joi.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
-const birthRegEx = /^(19[0-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+const birthRegEx =
+  /^(19[0-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
 module.exports = {
   signupSchema: Joi.object({
     email: Joi.string()
@@ -8,26 +9,28 @@ module.exports = {
       .required()
       .error(new Error('이메일 형식이 올바르지 않습니다.')),
     name: Joi.string()
-      .min(2)
+      .pattern(new RegExp('[^가-힣a-zA-Z]'))
+      .min(1)
+      .max(5)
       .required()
       .error(
-        new Error(
-          '이름은 최소 2자 이상, 알파벳 대소문자, 숫자로 구성되어야 합니다.'
-        )
+        new Error('이름은 최소 1~5자, 한글 또는 영문으로 구성되어야 합니다.')
       ),
     password: Joi.string()
-      .min(4)
+      .min(8)
+      .max(20)
       .required()
-      .error(new Error('비밀번호는 최소 4자 이상이어야 합니다.')),
+      .error(new Error('비밀번호는 4자 이상 20자 이하로 입력해주세요.')),
     confirm: Joi.string()
-      .min(4)
+      .min(8)
+      .max(20)
       .required()
-      .error(new Error('비밀번호는 최소 4자 이상이어야 합니다.')),
+      .error(new Error('비밀번호는 4자 이상 20자 이하로 입력해주세요.')),
     gender: Joi.string(),
-    birth : Joi.string()
-      .pattern(birthRegEx).required()
-      .error(new Error('생일형식을 확인해주세요'))
-
+    birth: Joi.string()
+      .pattern(birthRegEx)
+      .required()
+      .error(new Error('생일형식을 확인해주세요')),
   }),
 
   loginSchema: Joi.object({
@@ -36,9 +39,8 @@ module.exports = {
       .required()
       .error(new Error('이메일 또는 패스워드를 확인해주세요.')),
     password: Joi.string()
-      .min(4)
+      .min(8)
       .required()
       .error(new Error('이메일 또는 패스워드를 확인해주세요.')),
-  })
+  }),
 };
-
