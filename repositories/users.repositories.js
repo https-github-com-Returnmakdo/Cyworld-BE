5bd10848bbd4e54e5b70102cd83f686f741def67
JangKroed <didlsdydgh@gmail.com>
2022-10-31 22:57:10 +0900

    node-schedule 테스트를 위한 기존 today초기화 기능 주석처리

--- repositories/users.repositories.js
+++ repositories/users.repositories.js
@@ -60,11 +60,11 @@ class UsersRepositories {
     await Users.increment({ today: 1, total: 1 }, { where: { userId } });
   };
 
-  newTodayTotal = async ({ ip, time, userId }) => {
-    await MyHomeCounts.update({ time }, { where: { ip, userId } });
-    await Users.increment({ total: 1 }, { where: { userId } });
-    await Users.update({ today: 1 }, { where: { userId } });
-  };
+  // newTodayTotal = async ({ ip, time, userId }) => {
+  //   await MyHomeCounts.update({ time }, { where: { ip, userId } });
+  //   await Users.increment({ total: 1 }, { where: { userId } });
+  //   await Users.update({ today: 1 }, { where: { userId } });
+  // };
 
   introUpdate = async (userId, intro) => {
     const introupdate = await Users.update(
