6f9ccdcbc45d691bd879753407101a4f8d4fc9de
JangKroed <didlsdydgh@gmail.com>
2022-10-31 09:22:19 +0900

    ilchonpyung controllers response update

--- controllers/ilchonpyungs.controllers.js
+++ controllers/ilchonpyungs.controllers.js
@@ -5,12 +5,10 @@ class IlchonpyungsController {
 
   createBest = async (req, res, next) => {
     try {
-      const result = await this.ilchonpyungsService.createBest(req, res);
-      res.status(200).json({ data: result });
+      await this.ilchonpyungsService.createBest(req, res);
+      res.status(200).send({ msg: '일촌평이 작성되었습니다.' });
     } catch (error) {
-      res
-        .status(error.status || 400)
-        .send({ ok: false, message: error.message });
+      res.status(error.status || 400).send({ ok: false, msg: error.message });
     }
   };
 
@@ -19,9 +17,7 @@ class IlchonpyungsController {
       const result = await this.ilchonpyungsService.getBests(req, res);
       res.status(200).json({ data: result });
     } catch (error) {
-      res
-        .status(error.status || 400)
-        .send({ ok: false, message: error.message });
+      res.status(error.status || 400).send({ ok: false, msg: error.message });
     }
   };
 
@@ -30,9 +26,7 @@ class IlchonpyungsController {
       await this.ilchonpyungsService.deleteBest(req, res);
       res.status(200).send({ msg: '일촌평이 삭제되었습니다.' });
     } catch (error) {
-      res
-        .status(error.status || 400)
-        .send({ ok: false, message: error.message });
+      res.status(error.status || 400).send({ ok: false, msg: error.message });
     }
   };
 }
