81d2c3536af6a1d36bac889607d36c324a006726
JangKroed <didlsdydgh@gmail.com>
2022-10-31 10:58:45 +0900

    joi name 한글, 영문 최대 5자 제한 및 password 8자 이상 20자 이하 영문 숫자 특수문자 입력제한

--- util/joi.js
+++ util/joi.js
@@ -1,6 +1,8 @@
 const Joi = require('joi');
 const birthRegEx =
   /^(19[0-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+const nameRegEx = /^[가-힣|a-z|A-Z|]+$/;
+const passwordRegEx = /^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/
 module.exports = {
   signupSchema: Joi.object({
     email: Joi.string()
@@ -9,23 +11,20 @@ module.exports = {
       .required()
       .error(new Error('이메일 형식이 올바르지 않습니다.')),
     name: Joi.string()
-      .pattern(new RegExp('[^가-힣a-zA-Z]'))
-      .min(1)
       .max(5)
+      .pattern(nameRegEx)
       .required()
       .error(
         new Error('이름은 최소 1~5자, 한글 또는 영문으로 구성되어야 합니다.')
       ),
     password: Joi.string()
-      .min(8)
-      .max(20)
+      .pattern(passwordRegEx)
       .required()
-      .error(new Error('비밀번호는 4자 이상 20자 이하로 입력해주세요.')),
+      .error(new Error('비밀번호는 8자 이상 20자 이하로 입력해주세요.')),
     confirm: Joi.string()
-      .min(8)
-      .max(20)
+      .pattern(passwordRegEx)
       .required()
-      .error(new Error('비밀번호는 4자 이상 20자 이하로 입력해주세요.')),
+      .error(new Error('비밀번호는 8자 이상 20자 이하로 입력해주세요.')),
     gender: Joi.string(),
     birth: Joi.string()
       .pattern(birthRegEx)
